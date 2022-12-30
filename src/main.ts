456147b1f14204f6f6641e0c63e4b278404d7df8
bakedpotatolord <918780@dpsk12.net>
2022-12-29 23:21:43 -0700

    modify pretty much everything again

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,11 @@ import BFS from "./bfs"
 import rdfs from "./rdfs"
 
 let c = document.querySelectorAll("canvas")[0]
-let c2 = document.querySelectorAll("canvas")[1]
 let ctx = c.getContext("2d")
-let ctx2 = c2.getContext("2d")
 let form = document.forms[0]
 ctx.fillStyle = "black"
 
-ctx.globalCompositeOperation = 'lighter'
-ctx2.globalCompositeOperation = 'copy'
+
 
 export let cw:number
 export let ch:number 
@@ -26,13 +23,22 @@ let endingNode:Node;
 
 let parsedData:[number,number,number]
 
-function setup(heigth:number,width:number,blockSizeP:number){
+let mazeExists = false
+
+async function setup(heigth:number,width:number,blockSizeP:number){
+  if(heigth * width /blockSizeP > 16_000){
+    if(!confirm('Making large mazes like this can take A REALLY LONG TIME.\r\n\r\nDo you want to continue?')){
+      throw [false]
+    }else{
+      alert('Suit yourself, but dont say I didnt warn you.\r\n\r\nIf your browser says that it is unresponsive, chose "wait", as the is just thinking really hard, and not broken')
+    }
+  }
   if( width % blockSizeP == 0 && width % blockSizeP == 0){
-    
-    cw = c.width =c2.width= width
-    ch = c.height= c2.height =heigth
+    cw = c.width = heigth
+    ch = c.height =width
   
     blockSize = blockSizeP
+
   }else{
     throw [true,new Error('Width and Heigth must be a multiple of blockSize')]
   }
@@ -60,42 +66,17 @@ function draw(){
   console.log("started")
 
   ctx.fillStyle  = 'white'
-  ctx2.fillStyle  = 'white'
 
   ctx.clearRect(0,0,cw,ch)
   ctx.strokeStyle = 'black'
   ctx.fillRect(0,0,cw,ch)
   ctx.strokeRect(0,0,cw,ch)
-
-  ctx2.clearRect(0,0,cw,ch)
-  ctx2.strokeStyle = 'black'
-  ctx2.fillRect(0,0,cw,ch)
-  ctx2.strokeRect(0,0,cw,ch)
-  
-  
   
   nodes.forEach(el=>el.draw(ctx,blockSize))
   
-  let result = BFS(startingNode,endingNode,nodes,blockSize,false) 
-  
-  if(result ){
-    let n = endingNode
-    
-    while(n.parent != undefined){
-      ctx.lineWidth = 3
-      ctx.strokeStyle = 'blue'
-      ctx.beginPath()
-      ctx.moveTo(n.x,n.y)
-      ctx.lineTo(n.parent.x,n.parent.y)
-      ctx.stroke()
-      n = n.parent
-    }
+  BFS(startingNode,endingNode,nodes,blockSize,false) 
 
-    nodes.forEach(el=>el.draw(ctx2,blockSize))
-  }else{
-    //console.log('retrying')
-    requestAnimationFrame(()=>setup(...parsedData))
-  }
+  mazeExists = true
 }
 
 
@@ -103,9 +84,9 @@ form.onsubmit= (e)=>{
   e.preventDefault()
   let data = (new FormData(<HTMLFormElement>e.target))
   //@ts-ignore
-  parsedData = Array.from(data.entries()).map(el=>el[1])
-  try{
+  parsedData = Array.from(data.entries()).map(el=>parseFloat(el[1]))
 
+  try{
     setup(...parsedData)
   }catch(err){
     if(err[0]) alert(err[1])
@@ -114,20 +95,35 @@ form.onsubmit= (e)=>{
   }
 }
 
-(<HTMLButtonElement>document.querySelector('#d')).onclick = ()=> download(false);
-(<HTMLButtonElement>document.querySelector('#ds')).onclick = ()=> download(true);
-
-export function download(withSol:boolean){
-  let a = document.createElement('a')
-  document.body.appendChild(a)
-  if(withSol){
+(<HTMLButtonElement>document.querySelector('#d'))
+.onclick = (e)=>{
+  e.preventDefault()
+  if(mazeExists){
+    let a = document.createElement('a')
+    document.body.appendChild(a)
     a.href = c.toDataURL()
-    a.download = 'mazeSolution.png'
-  }else{
-    a.href = c2.toDataURL()
     a.download = 'maze.png'
+    a.click()
+  }else{
+    alert('you need to generate your maze first bozo')
   }
-  a.click()
 }
 
-export{}
+(<HTMLButtonElement>document.querySelector('#showSolution'))
+.onclick = (e)=>{
+  e.preventDefault()
+  if(mazeExists){
+    let n = endingNode
+    ctx.beginPath()
+    ctx.moveTo(n.x,n.y)
+    while(n.parent != undefined){
+      ctx.lineWidth = blockSize * (3/40)
+      ctx.strokeStyle = 'blue'
+      ctx.lineTo(n.parent.x,n.parent.y)
+      n = n.parent
+    }
+    ctx.stroke()
+  }else{
+    alert('you need to generate your maze first bozo')
+  }
+}
