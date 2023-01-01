e611ea415da7f7bcd32879be9aac5782f103eccc
= <918780@dpsk12.net>
2023-01-01 14:46:04 -0700

    get formdata directly

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,29 @@
-
 import Node from "./Node"
 import BFS from "./bfs"
 import rdfs from "./rdfs"
-import { makeNodeMap} from "./helpers"
+import { getEndingNode, getStartingNode, makeNodeMap} from "./helpers"
 
 let c = document.querySelector("canvas")
 let ctx = c.getContext("2d")
 let form = document.forms[0]
 ctx.fillStyle = "black"
 
+// declare canvas vars
+let cw:number
+let ch:number 
+let blockSize:number
 
-
-export let cw:number
-export let ch:number 
-
-
-export let fill:number
-export let blockSize:number
-
+// declare graph algorithm vars
 let nodes:Map<string,Node>
 let startingNode:Node;
 let endingNode:Node;
 
-let parsedData:[number,number,number]
+
 
 let mazeExists = false
 
 function setup(heigth:number,width:number,blockSizeP:number){
+  //form validation
   if(heigth * width > 500_000){
     if(!confirm('Making large mazes like this can take A REALLY LONG TIME.\r\n\r\nDo you want to continue?')){
       throw [false]
@@ -34,7 +31,7 @@ function setup(heigth:number,width:number,blockSizeP:number){
       alert('Suit yourself, but dont say I didnt warn you.\r\n\r\nIf your browser says that it is unresponsive, chose "wait", as the is just thinking really hard, and not broken')
     }
   }
-
+  //more form validation
   if( width % blockSizeP == 0 && width % blockSizeP == 0){
     if(width >= blockSizeP && heigth >= blockSizeP){
       cw = c.width = heigth
@@ -43,25 +40,21 @@ function setup(heigth:number,width:number,blockSizeP:number){
     }else{
       throw [true,new Error('Width and Heigth must be greater than blockSize')]
     }
-
   }else{
     throw [true,new Error('Width and Heigth must be a multiple of blockSize')]
   }
 
   nodes = makeNodeMap(cw,ch,blockSize) 
 
-  startingNode = Array.from(nodes.entries())[0][1]
+  startingNode = getStartingNode(nodes)
   startingNode.isStartingNode = true
   
-  endingNode = Array.from(nodes.entries())
-  .slice(-1)[0][1]
+  endingNode = getEndingNode(nodes)
   endingNode.isEndingNode = true
   rdfs(nodes,startingNode,blockSize)
 
   draw()
 }
-
-
 
 function draw(){
   console.log("started")
@@ -77,9 +70,8 @@ function draw(){
   ctx.stroke()
   
   nodes.forEach(el=>el.draw(ctx,blockSize))
-
+  //use breadth-first search because depth first will find "a solutoion", but not "the" solutoin  
   BFS(startingNode,endingNode,nodes,blockSize,false) 
-
   mazeExists = true
 }
 
@@ -88,10 +80,13 @@ form.onsubmit= (e)=>{
   e.preventDefault()
   let data = (new FormData(<HTMLFormElement>e.target))
   //@ts-ignore
-  parsedData = Array.from(data.entries()).map(el=>parseFloat(el[1]))
 
   try{
-    setup(...parsedData)
+    setup(
+      parseFloat(data.get('width').toString()),
+      parseFloat(data.get('heigth').toString()),
+      parseFloat(data.get('blockSize').toString()),
+    )
   }catch(err){
     if(err[0]) alert(err[1])
     console.log(err)
