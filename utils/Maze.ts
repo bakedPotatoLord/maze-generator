96693278175f9537de61a0175e5e8b2368766f77
bakedpotatolord <jeffy9745@gmail.com>
2023-09-15 16:18:58 -0700

    general code cleanup

--- utils/Maze.ts
+++ utils/Maze.ts
@@ -37,10 +37,10 @@ export default class Maze{
 
         this.nodes.forEach(n=>{
             n.walls ={
-                left: n.wallsTo.filter(no=>no.x == n.x-20 && no.y ==n.y).length > 0 || n.x == blocksize/2,
-                right: n.wallsTo.filter(no=>no.x == n.x+20 && no.y ==n.y).length>0 || n.x == this.cw - blocksize/2,
-                top: n.wallsTo.filter(no=>no.x == n.x && no.y ==n.y-20).length>0 || n.y == blocksize/2,
-                bottom: n.wallsTo.filter(no=>no.x == n.x && no.y ==n.y+20).length>0|| n.y == this.ch - blocksize/2,
+                left:( n?.wallsTo?.filter(no=>no.x == n.x-20 && no.y ==n.y)?.length?? 0) > 0 || n.x == blocksize/2,
+                right:( n?.wallsTo?.filter(no=>no.x == n.x+20 && no.y ==n.y)?.length?? 0)>0 || n.x == this.cw - blocksize/2,
+                top:( n?.wallsTo?.filter(no=>no.x == n.x && no.y ==n.y-20)?.length?? 0)>0 || n.y == blocksize/2,
+                bottom:( n?.wallsTo?.filter(no=>no.x == n.x && no.y ==n.y+20)?.length?? 0) >0|| n.y == this.ch - blocksize/2,
             }
         })  
         console.log("maze constructed successfully")
@@ -49,12 +49,12 @@ export default class Maze{
     draw(ctx:CanvasRenderingContext2D){
 		ctx.fillStyle  = 'white'
         ctx.clearRect(0,0,this.cw,this.ch)
-        ctx.strokeStyle = 'black'
+        ctx.strokeStyle = 'purple'
         ctx.lineWidth = 3
         ctx.fillRect(0,0,this.cw,this.ch)
-        ctx.beginPath
-        ctx.rect(1,1,this.cw-1,this.ch-1)
-        ctx.stroke()
+        // ctx.beginPath
+        // ctx.rect(1,1,this.cw-1,this.ch-1)
+        // ctx.stroke()
         //draw all nodes
         this.nodes.forEach(el=>el.draw(ctx,this.blocksize))
         //use breadth-first search because depth first will find "a" solutoion, but not "the" solutoin  
