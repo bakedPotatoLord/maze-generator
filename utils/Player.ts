b451086f2db04e58d3382309d67a4b8a3ffa4b12
bakedpotatolord <jeffy9745@gmail.com>
2023-09-20 19:38:27 -0600

    add player reset function

--- utils/Player.ts
+++ utils/Player.ts
@@ -3,7 +3,7 @@ export default class Player{
 	y=0;
 	r: number;
 	
-	constructor(x=0,y=0,r=0){
+	constructor(x=10,y=10,r=8){
 		this.x = x;
 		this.y = y;
 		this.r = r;
@@ -23,5 +23,11 @@ export default class Player{
 		ctx.beginPath()
 		ctx.arc(this.x,this.y,this.r-1,0,this.TAU)
 		ctx.fill()
+	}
+
+	reset(){
+		this.x= 10
+		this.y= 10
+		this.r= 8
 	}
 }
